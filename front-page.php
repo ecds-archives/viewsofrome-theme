2e7bbeeeceedbbef171d1ad43dc317f14ecdc215
Kyle Bock <kylewbock@gmail.com>
2012-04-09 16:22:26 -0400

    map manager and ajax calls, works for a single overlay at a time without insert into the database

--- front-page.php
+++ front-page.php
@@ -18,8 +18,43 @@
   }
 
   Seadragon.Utils.addEvent(window, 'load', init);
+  var $ = jQuery.noConflict();
+  $(document).ready(function() {
+    $('#hide').live('click', function() {
+        $('#mapOverlay').hide();
+    });
+    $('#load').live('click', function() {
+        $.ajax({
+            url: '/wp-admin/admin-ajax.php',
+            type: 'GET',
+            data: {action: 'get_overlay_data'},
+            success: function(results) {
+                alert(results);
+            }
+        });
+    });
+  });
 </script>
 
-<div id='map'></div>
+<div id="mapContainer">
+    <div id='map'></div>
+    <div id='mapOverlay'>
+        <div id="overlay-title">
+            <h2>The Colosseum</h2>
+            <?php
+                $arr = array(
+                    'category'  => "Arena",
+                    'pageId'    => '/colosseum/'
+                );
+
+                echo json_encode($arr);
+            ?>
+        </div>
+        <div>
+            <a id="hide" href="#">Hide</a><br />
+            <a id="load" href="#">Load</a>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -4,5 +4,35 @@ wp_register_script('seadragon', get_stylesheet_directory_uri() . '/js/seadragon-
 wp_register_script('raphael', get_stylesheet_directory_uri() . '/js/raphael-min.js', array());
 wp_register_script('seajax', get_stylesheet_directory_uri() . '/js/seajax-utils.js', array('seadragon', 'raphael'));
 
+function get_overlay_data() {
+    if (isset($_GET['data'])) {
+        $output = "";
 
+        //validate somehow?
+        $output .= json_encode($_GET['data']);
+            
+        echo $output;
+    }
+    die(0);
+//    return $output;
+}
+
+function post_overlay_data() {
+    echo $_POST['data'];
+    if(isset($_POST['data'])) {
+        $data = $_POST['data'];
+        $output = "";
+        
+        $output .= "Title: " . $data['title'] . "<br>";
+        $output .= "ID: " . $data['id'] . "<br>";
+
+        echo $output;
+    }
+
+    return 0;
+}
+
+add_action('wp_ajax_post_overlay_data', 'post_overlay_data');
+add_action('wp_ajax_get_overlay_data', 'get_overlay_data');
+add_action('wp_ajax_nopriv_get_overlay_data', 'get_overlay_data');
 ?>
